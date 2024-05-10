4d850cc779ef1aff2c15fe903909dc157e9ebca2
Abdalrahman-Ashraf <161497698+Abdalrahman-Ashraf@users.noreply.github.com>
2024-05-10 23:43:16 +0300

    fixed navbar resizing

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -7,8 +7,8 @@ import { useNavigate } from "react-router-dom";
 function NavBar() {
   const nav = useNavigate();
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" style={{ height: '70px'}}>
-      <Container style={{ margin: '0', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+    <Navbar bg="dark" variant="dark" expand="lg" style={{ height: '70px' }}>
+      <Container style={{ maxWidth: '100%' }}>
         <Navbar.Brand href="/Home" style={{ paddingLeft: '2vw', paddingRight: '1vw', marginRight: '1.5vw', marginLeft: '2vw' }}>
           <img
             src={logo}
@@ -19,20 +19,18 @@ function NavBar() {
           {' Imperium'}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav" style={{ margin: '0' }}>
           <Nav className="ml-auto" style={{ margin: '0' }}>
             <Nav.Link href="/Home" className="nav-link" style={{ paddingLeft: '1vw', paddingRight: '1vw', margin: '0' }}>Home</Nav.Link>
             <Nav.Link href="/donor" className="nav-link" style={{ paddingLeft: '1vw', paddingRight: '1vw', margin: '0' }}>Donate</Nav.Link>
             <Nav.Link href="#faqs" className="nav-link" style={{ paddingLeft: '1vw', paddingRight: '1vw', margin: '0' }}>FAQs</Nav.Link>
             {/* Add more Nav.Link components for additional pages */}
           </Nav>
-
-        </Navbar.Collapse>
-        <div style={{ display:'flex', justifyContent: 'flex-end' }}>
-          <Button type="button" onClick={()=>nav("/Login")} variant="outline-light" 
-          style={{ paddingLeft: '1.2vw', paddingRight: '1.2vw', paddingTop: '3px', paddingBottom: '5px', marginRight:'-25vw'}}>
+          <Button className="ms-auto" type="button" onClick={()=>nav("/Login")} variant="outline-light" 
+          style={{ paddingLeft: '1.2vw', paddingRight: '1.2vw', paddingTop: '3px', paddingBottom: '5px', marginRight: '4vw' }}>
             Sign In</Button>
-        </div>
+        </Navbar.Collapse>
+          
       </Container>
     </Navbar>
   );
